50d838005d6fa0f0ef1d7c78f8d2ec7a87150a8b
slazyplayz <slazy06bg@gmail.com>
2023-11-23 12:16:51 +0200

    reordered files and fixed all-recipes.html

---
+++ js/dropdown.js
@@ -0,0 +1,5 @@
+const recipesDropdown = document.getElementById('recipes-dropdown');
+
+recipesDropdown.addEventListener('click', () => {
+    window.location = "/pages/recipe/recipes-all.html"
+});
